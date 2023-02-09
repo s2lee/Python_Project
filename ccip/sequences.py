053daaea90396b395de4f7dd06c13501958aca9d
s2lee <therok.manager@gmail.com>
2023-02-09 20:01:58 +0900

    Fix sequences

--- ccip/sequences.py
+++ ccip/sequences.py
@@ -10,12 +10,3 @@ class Books(Sequence):
 
     def __getitem__(self, item):
         return self._values.__getitem__(item)
-
-
-books = Books("a", "b", "c")
-print(len(books))
-for book in books:
-    print(book)
-
-print(books)
-print(books[1:3])
